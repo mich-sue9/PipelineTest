a3372b958e67e5f9d6c34a870b0d93ff6fef4060
Michelle <mich.sue9@gmail.com>
2021-08-10 15:00:53 -0700

    Added tests

--- src/test/java/com/michelle/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/michelle/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,18 @@ package com.michelle.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CalculatorApplicationTests {
 
+	private Calculator calculator = new Calculator();
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertEquals(5, calculator.sum(2,3));
 	}
 
 }
+
+// https://mkyong.com/gradle/junit-5-gradle-examples/
